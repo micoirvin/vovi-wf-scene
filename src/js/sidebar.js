745b9d52c4673f73ca3505f2af4104dc376bcad1
Mico Irvin De Mesa <104616752+micoirvin@users.noreply.github.com>
2026-02-16 20:58:33 +0800

    sidebar mobile

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,6 +1,15 @@
 function sidebar() {
-  const sideMenu = document.querySelector('.side_menu');
-  if (!sideMenu) return;
+  const side = document.querySelector('.side');
+  if (!side) return;
+  const sideMenu = side.querySelector('.side_menu');
+
+  const burger = side.querySelector('.side_burger-btn');
+  burger.addEventListener('click', () => {
+    side.classList.toggle('is-closed');
+    sideMenu.classList.remove('is-closed');
+    sideMenu.classList.add('is-open');
+  });
+
   const sideMenuHead = sideMenu.querySelector('.side_menu-head');
   sideMenuHead.addEventListener('click', () => {
     if (sideMenu.classList.contains('is-closed')) {
