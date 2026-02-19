464600846b183b48b52724d1ff37fdb5bda0137d
Mico Irvin De Mesa <104616752+micoirvin@users.noreply.github.com>
2026-02-19 21:33:43 +0800

    Finsweet reload case for sidebar

--- src/js/handleFinsweet.js
+++ src/js/handleFinsweet.js
@@ -1,6 +1,52 @@
 function handleFinsweet() {
-  // window.FinsweetAttributes.destroy(); // NO NEED??
-  window.FinsweetAttributes.load('list');
+  // REFACTOR. MODULARIZE THIS
+
+  const [sideCsPreCleanup, sideCsCleanup] = sideCsCase();
+
+  setTimeout(() => {
+    // window.FinsweetAttributes.destroy();
+    // window.FinsweetAttributes.load('list');
+    sideCsPreCleanup();
+
+    window.FinsweetAttributes.modules.list.restart().then((res) => {
+      sideCsCleanup();
+    });
+  }, 300);
+}
+
+function sideCsCase() {
+  const sideCs = document.querySelector('.side_cs');
+  const clearEls = sideCs.querySelectorAll('[fs-list-element="clear"]');
+  const sidebarCs = document.querySelector(
+    '[fs-list-element="filters"][fs-list-instance="sidebar-cs"]'
+  );
+
+  const tempClone = sideCs.cloneNode(true);
+  tempClone.querySelectorAll('[fs-list-element][fs-list-instance]').forEach((t) => {
+    t.removeAttribute('fs-list-element');
+    t.removeAttribute('fs-list-instance');
+  });
+
+  sideCs.style.display = 'none';
+  sideCs.insertAdjacentElement('afterend', tempClone);
+  // tempClone.style.display = '';
+
+  const checkeds = sidebarCs.querySelectorAll('input:checked');
+  clearEls.forEach((c) => c.click());
+  return [
+    () => {
+      checkeds.forEach((c) => {
+        c.checked = 'checked';
+      });
+    },
+    () => {
+      setTimeout(() => {
+        // tempClone.style.display = 'none';
+        sideCs.style.display = '';
+        tempClone.remove();
+      }, 300);
+    },
+  ];
 }
 
 export { handleFinsweet };
